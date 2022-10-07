841d7252d153c69b2c33ff29380e52eb2e9b3baf
hoangth1992 <hoangth@yopaz.vn>
2022-10-07 18:14:21 +0700

    chapter 4: model

---
+++ app/models/sight.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: sights
+#
+#  id            :bigint           not null, primary key
+#  activity_type :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  place_id      :bigint           not null
+#
+# Indexes
+#
+#  index_sights_on_place_id  (place_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (place_id => places.id)
+#
+class Sight < ApplicationRecord
+  ACTIVITY_TYPES = [CHECKIN = "checkin", CHECKOUT = "checkout"].freeze
+
+  belongs_to :place
+  has_many :posts, as: :postable
+
+  validates :activity_type, presence: true, inclusion: {in: ACTIVITY_TYPES}
+end
---
+++ app/models/user.rb
@@ -0,0 +1,30 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  email      :string           not null
+#  first_name :string           not null
+#  is_public  :boolean          default(TRUE), not null
+#  last_name  :string
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email) UNIQUE
+#  index_users_on_username  (username) UNIQUE
+#
+class User < ApplicationRecord
+  validates :email, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :username, uniqueness: true
+  validates :first_name, presence: true
+
+  has_many :posts
+  has_many :bonds
+  has_many :inward_bonds, class_name: 'Bond', foreign_key: :friend_id
+  has_many :followings, -> {Bond.following}, through: :bonds, source: :friend
+  has_many :follow_requests, -> {Bond.requesting}, through: :bonds, source: :friend
+  has_many :followers, -> {Bond.following}, through: :inward_bonds, source: :user
+end
