13d2a2e0e25615f987ec1d838f5ea44b99443cfc
hoangth1992 <hoangth@yopaz.vn>
2022-10-24 13:33:43 +0700

    chapter 10: elastic beanstalk

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,15 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+
+
+rails_env = ENV['RAILS_ENV'] || "production"
+if rails_env == "production"
+  bind "unix:///var/run/puma/my_app.sock"
+  pidfile "/var/run/puma/my_app.sock"
+else
+  pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+end
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
